09a14fe54eafb8914924f9e7928e1ac916686515
PremKumarAK47 <premkumar147369@gmail.com>
2023-05-07 02:55:13 +0530

    Dao

--- CrimeManagemntApplication/src/dao/CrimeDao.java
+++ CrimeManagemntApplication/src/dao/CrimeDao.java
@@ -1,6 +1,14 @@
 package dao;
 
+import beans.CrimeInfo;
+import beans.CriminalInfo;
+import exception.CrimeException;
+import exception.CriminalException;
+
 public interface CrimeDao {
 	
+	public String registerCrime(CrimeInfo crm) throws CrimeException;
+	
+	public String registerCriminal(CriminalInfo criminalInfo) throws CriminalException;
 
 }
--- CrimeManagemntApplication/src/dao/CrimeDaoImpl.java
+++ CrimeManagemntApplication/src/dao/CrimeDaoImpl.java
@@ -1,5 +1,22 @@
 package dao;
 
-public class CrimeDaoImpl {
+import beans.CrimeInfo;
+import beans.CriminalInfo;
+import exception.CrimeException;
+import exception.CriminalException;
+
+public class CrimeDaoImpl implements CrimeDao {
+
+	@Override
+	public String registerCrime(CrimeInfo crm) throws CrimeException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String registerCriminal(CriminalInfo criminalInfo) throws CriminalException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
